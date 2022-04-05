ef1d5a2f8afcdf1203901b0437d5ff722dea2793
Londonexmachina <london.ex.machina@gmail.com>
2022-04-04 19:28:13 -0700

    Passed tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function(arr, arg1, arg2) {
-
+const removeFromArray = function(arr, ...args) {
+    let newArray = [...arr];
+    for (i = 0; i < arr.length; i++) {
+        if (newArray.includes(args[i])) {
+            newArray.splice(newArray.indexOf(args[i]), 1)
+        }    
+    }
+    return newArray;
 };
 
 // Do not edit below this line
