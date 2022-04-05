3223729d76604cf90955b8d1510a732de6c4e1db
Londonexmachina <london.ex.machina@gmail.com>
2022-04-04 20:56:06 -0700

    Write functions

--- 05_sumAll/complicatedSumAll.js
+++ 05_sumAll/complicatedSumAll.js
@@ -1,5 +1,23 @@
 const sumAll = function(num1, num2) {
     let j = 0;
+    if (num2 > num1 && Math.sign(num1) == 1 && Number.isInteger(num2)) {
+        for (let i = num1; i < (num2 + 1); i++) {
+            j += i;
+        }
+        return j;
+    } else if (num1 > num2) {
+        for (let i = num1; i > (num2 - 1); i--) {
+            j += i;
+        }
+        return j;
+    } else {
+        return "ERROR"
+    }
+};
+
+
+const sumAll = function(num1, num2) {
+    let j = 0;
     if (Math.sign(num1) == 1 && Number.isInteger(num2)) {
         if (num2 > num1) {
             for (let i = num1; i < (num2 + 1); i++) {
@@ -18,5 +36,4 @@ const sumAll = function(num1, num2) {
 };
 
 
-// Do not edit below this line
-module.exports = sumAll;
+
