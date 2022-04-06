0f4bf497749fad3b0264ead900c68a5a53d1aaee
Londonexmachina <london.ex.machina@gmail.com>
2022-04-05 21:10:11 -0700

    Create functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+    let celsius = (fahrenheit - 32) * .5556;
+    celsius = Math.round(celsius * 10) / 10;
+    return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+    let fahrenheit = (celsius * 1.8) + 32;
+    fahrenheit = Math.round(fahrenheit * 10) / 10;
+    return fahrenheit;
 };
 
 // Do not edit below this line
